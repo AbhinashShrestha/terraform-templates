a7a03a72260e4ede665409737822292665f7619f
Anon <oredev@proton.me>
2024-09-12 11:18:26 +0545

    iam policy and user creation with specific permission

--- iam_project/main.tf
+++ iam_project/main.tf
@@ -13,9 +13,43 @@ provider "aws" {
 }
 
 locals {
-	users_config_file = file("./users.yaml")
+	users_config_file = yamldecode(file("./users.yaml")).users
+	user_role_pair = flatten([for user in local.users_config_file: [for role in user.roles: {
+		username = user.username
+		role = role
+	}]])
+}
+
+resource "aws_iam_user" "users" {
+	for_each = toset(local.users_config_file[*].username)
+	name = each.value 
+}
+
+
+resource "aws_iam_user_login_profile" "iam-config" {
+  for_each = aws_iam_user.users
+  user     = each.value.name
+  password_length = 12
+  pgp_key = "keybase:some_person_that_exists"
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
+}
+
+#Attaching policy
+resource "aws_iam_user_policy_attachment" "iam-policy" {
+	for_each = {
+		for pair in local.user_role_pair :
+		"${pair.username}-${pair.role}" => pair
+	}
+	user 	   = aws_iam_user.users[each.value.username].name
+	policy_arn = "arn:aws:iam::aws:policy/${each.value.role}"
 }
 
 output "output" {
-	value = local.users_config_file
+	value = local.user_role_pair
 }
