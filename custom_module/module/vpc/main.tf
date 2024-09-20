247031d2ea78134bda7020a7b7f1b1943a998267
owner <oredev@proton.me>
2024-09-20 13:59:52 +0545

    custom module done

--- custom_module/module/vpc/main.tf
+++ custom_module/module/vpc/main.tf
@@ -19,10 +19,29 @@ locals {
 	public_subnet = {
 		for key,config in var.subnet_config: key => config if config.public
 	}
+	private_subnet = {
+                for key,config in var.subnet_config: key => config if !config.public
+        }
+
 }
 
 resource "aws_internet_gateway" "igw" {
 	vpc_id = aws_vpc.vpc.id
 	count = length(local.public_subnet) > 0 ? 1 : 0
-
 }
+
+resource "aws_route_table" "rwt" {
+	count = length(local.public_subnet) > 0 ? 1 : 0
+	vpc_id = aws_vpc.vpc.id
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.igw[0].id
+	}
+}
+
+resource "aws_route_table_association" "route-association" {
+	for_each = local.public_subnet
+	subnet_id = aws_subnet.subnet[each.key].id
+	route_table_id = aws_route_table.rwt[0].id
+}
+
